1da7a7509e0fc15bf4dfb84265a871ab35d19f7c
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-04-02 13:02:10 +0700

    nama variabel untuk struktur detail alamat

---
+++ strukturD 3 lagi/strukturD 3 lagi.cpp
@@ -0,0 +1,15 @@
+// strukturD 3 lagi.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+    string desa;
+    string kota;
+};
+
+int main()
+{
+    
+}
