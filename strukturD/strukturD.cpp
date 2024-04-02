4a915c5dd97b10b349eae16df28bdbefdbf1ca23
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-04-02 12:15:41 +0700

    membuat fungsi main dari structur mahasiswa

--- strukturD/strukturD.cpp
+++ strukturD/strukturD.cpp
@@ -14,6 +14,15 @@ struct mahasiswa {
 int main()
 {
     mahasiswa mhs;
+    cout << "Nomor Mahasiswa : " ;
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa : ";
+    cin >> mhs.alamat;
+    cout << "Umur Mahasiswa :";
+    cin >> mhs.umur;
+
     
 }
 
