deb210ace26bf2b25e20b22d9c64a4a62b921be2
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-04-02 12:25:06 +0700

    membuat struktur detail alamat

---
+++ StrukturD 2/StrukturD 2.cpp
@@ -0,0 +1,15 @@
+// StrukturD 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+
+};
+
+int main()
+{
+    
+}
+
