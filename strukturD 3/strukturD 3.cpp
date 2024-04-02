2b133e8c277c0f8fd1ae17ef7fddc74c534b8e62
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-04-02 12:53:56 +0700

    membuat nama variabel untuk detail alamat

---
+++ strukturD 3/strukturD 3.cpp
@@ -0,0 +1,47 @@
+// strukturD 3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+    string desa;
+    string kota;
+};
+
+struct mahasiswa {
+    string nim;
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
+
+int main()
+{
+    mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+
+    cout << "Alamat Mahasiswa : " << endl;
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota : ";
+    cin >> mhs.alamat.kota;
+
+    cout << "Umur Mahasiswa :";
+    cin >> mhs.umur;
+
+
+    cout << endl;
+    cout << "\n NIM : " << mhs.nim;
+    cout << "\n Nama : " << mhs.nama;
+    cout << "\n Alamat : ";
+    cout << "\n Desa : " << mhs.alamat.desa;
+    cout << "\n Kota : " << mhs.alamat.kota;
+    cout << "\n Umur : " << mhs.umur;
+
+
+}
+
