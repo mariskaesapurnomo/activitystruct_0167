77d58c973ccb1fc6266581112a0bdfb5d3c7964b
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-04-02 13:24:16 +0700

    membuat nama variabel struktur detail alamat

---
+++ strukturD 4/strukturD 4.cpp
@@ -0,0 +1,15 @@
+// strukturD 4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct DetailAlamat {
+    string desa;
+    string kota;
+};
+
+int main() {
+
+}
