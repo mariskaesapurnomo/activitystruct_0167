bf2aecdc2da16855d6ea63f844546657d1b00522
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-04-02 12:42:39 +0700

    membuat fungsi main struktur baru detail alamat

--- StrukturD 2/StrukturD 2.cpp
+++ StrukturD 2/StrukturD 2.cpp
@@ -12,12 +12,34 @@ struct DetailAlamat {
 struct mahasiswa {
     string nim;
     string nama;
-    string alamat;
-    string umur;
+    DetailAlamat alamat;
+    int umur;
 };
 
 int main()
 {
+    mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+
+    cout << "Alamat Mahasiswa : " << endl;
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota : ";
+    cin >> mhs.alamat.kota;
+
+    cout << "Umur Mahasiswa :";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n NIM : " << mhs.nim;
+    cout << "\n Nama : " << mhs.nama;
+    cout << "\n Alamat : "; 
+    cout << "\n Desa : " << mhs.alamat.desa;
+    cout << "\n Kota : " << mhs.alamat.kota;
+    cout << "\n Umur : " << mhs.umur;
     
 }
 
